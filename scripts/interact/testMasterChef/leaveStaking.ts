061addb7e84d751ce24320b6523fd5c472cb196d
Warun S <warunsinx@gmail.com>
2023-10-09 10:19:48 +0700

    feat: :sparkles: add enterStaking, leaveStaking

--- scripts/interact/testMasterChef/leaveStaking.ts
+++ scripts/interact/testMasterChef/leaveStaking.ts
@@ -1,26 +1,26 @@
 import hre, { ethers } from "hardhat";
 import {
-  MockBEP20__factory,
   MasterChef__factory,
   CakeToken__factory,
 } from "../../../typechain-types";
 import addressUtils from "../../../utils/addressUtils";
 
-// npx hardhat run scripts/interact/testMasterChef/withdraw.ts --network bkc_test
+// npx hardhat run scripts/interact/testMasterChef/leaveStaking.ts --network bkc_test
 
 const main = async () => {
   const [, alice] = await ethers.getSigners();
   const addressList = await addressUtils.getAddressList(hre.network.name);
 
-  const lp1 = MockBEP20__factory.connect(addressList["LP1"], alice);
   const cake = CakeToken__factory.connect(addressList["CAKE"], alice);
   const masterChef = MasterChef__factory.connect(
     addressList["MasterChef"],
     alice
   );
 
-  await masterChef.withdraw(1, ethers.parseEther("20")).then((tx) => tx.wait());
-  console.log("Alice wihtdrawn 20 LP1");
+  await masterChef
+    .leaveStaking(ethers.parseEther("20"))
+    .then((tx) => tx.wait());
+  console.log("Alice winthdrawn 20 CAKE");
 
   console.log(
     "Alice's Cake balance:",
